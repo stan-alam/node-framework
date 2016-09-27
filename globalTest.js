7ae93ce5d4c8dcbe9c0aca94faefd2a2394de1a4
Stanislaw Alam <stan.alam@gmail.com>
2016-09-27 08:44:02 -0400

    Screenshots Browser Full Screen

--- globalTest.js
+++ globalTest.js
@@ -167,9 +167,11 @@ let runTestFramework = function(microservice){
                                   fs.writeFileSync('screenshots/' + name, data, 'base64');
                                 };
 
-//                                driver.takeScreenshot().then(function(data) {
-//                                  writeScreenshot(data, microservice+'_' + testCase+'ID_'+test.id+'Step_'+stepindex+'.png');
-//                                });
+                                if(driver.hasOwnProperty('takeScreenshot')){
+                                    driver.takeScreenshot().then(function(data) {
+                                      writeScreenshot(data, microservice+'_' + testCase+'ID_'+test.id+'Step_'+stepindex+'.png');
+                                    });
+                                }
 
                                 if(error){
                                     console.error("Step: "+stepindex);
