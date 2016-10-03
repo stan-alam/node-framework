dd9c50bc85e3d421fdd77c75278c8805f043f457
Stanislaw Alam <stan.alam@gmail.com>
2016-10-03 09:57:55 -0400

    Version: 0.5.0 - redo of GlobalTest File

--- globalTest.js
+++ globalTest.js
@@ -22,6 +22,8 @@ let runTestFramework = function(microservice){
 
     before(function(done) {
         this.timeout(500000000)
+
+        //Set UI config Information from environment file
         let uiConfig = {
             'username': envVars.credentials.username,
             'password': envVars.credentials.password,
@@ -30,14 +32,19 @@ let runTestFramework = function(microservice){
 
         uiFunctions.configLoadUi(uiConfig, function(uiDriver) {
             driver = uiDriver;
+
+            //If user is mbmormann call to clean out all config information
             if(envVars.credentials.username == 'mbmormann'){
+                //Call to delete all configuration information
                 preSetupLib.deleteAll(driver, function(driver){
+                    //Call to run pre setup with templates and applications
                     preSetupLib.setup(driver, function(driver, setup){
                         preSetup = setup;
                         done();
                     });
                 });
             }else{
+                //Call to run pre setup with templates and applications
                 preSetupLib.setup(driver, function(driver, setup){
                     preSetup = setup;
                     done();
@@ -48,6 +55,7 @@ let runTestFramework = function(microservice){
 
     after(function(done){
         this.timeout(800000000);
+        //Call to cleanup preSetup config applications
         preSetupLib.cleanUp(driver, preSetup, function(){
             driver.quit();
             done();
@@ -56,6 +64,8 @@ let runTestFramework = function(microservice){
 
     //Loop thought all the TestCases for Hub-admin-ui
     let testCasefiles = fs.readdirSync('./testStories/'+microservice+'/');
+
+    //If testStories has a develop.json only run that file and exit
     if(testCasefiles.indexOf('develop.json') !== -1){
         testCasefiles = ['develop.json'];
     }
@@ -66,19 +76,26 @@ let runTestFramework = function(microservice){
         console.error("Result: "+ JSON.stringify(validateResult));
 
         let ValidateResult = { text: '' };
+        //See if result is from api and is an array of responses [{body, statuscode}]
         if(Array.isArray(validateResult.text) && (validateResult.text.length > 0) && (validateResult.text[0].body)){
+            //Validate only the first response in array
             validateResult.text = validateResult.text[0];
         }
 
+        //Check if user is validating on a location in the responding data (multi levels of response: body.id)
         if((validate.test.location) && (validate.test.location.indexOf('.') > -1)){
+            //Validate on the exact location of object with in result
             let rval = _eval('module.exports = function () { return '+ JSON.stringify(validateResult.text)+'.'+validate.test.location+'} ');
             ValidateResult.text = rval();
         }else if(validate.test.location){
+            //If location is set with no children validate on first location ( location: body )
             ValidateResult.text = validateResult.text[validate.test.location];
         }else{
+            //No location is set for validation, validate on whole response
             ValidateResult.text = validateResult.text;
         }
-        
+
+        //If caseSensitive is set to false then lowercase validation and test
         if(('caseSensitive' in validate.test) && (validate.test.caseSensitive == false)){
             if(!Array.isArray(validate.test.value))
                 validate.test.value == validate.test.value.toLowerCase();
@@ -86,11 +103,13 @@ let runTestFramework = function(microservice){
                 ValidateResult.text = ValidateResult.text.toLowerCase();
         }
 
+        //Custom validation to see if response is array and empty
         if('isEmpty' in validate.test){
             assert.isAbove(result.text.length, 0);
             callback();
         }
 
+        //Run builtin chai assertions based on what is in validation.json
         if( availableAsserts.chaiTwo.indexOf(validate.test.action) !== -1){
                 assert[validate.test.action](ValidateResult.text, validate.test.name || '');
                 callback();
@@ -102,6 +121,7 @@ let runTestFramework = function(microservice){
                  callback();
          }else{
             //List of Extra lookups other then chai
+            //Custom use of action operator where you can see if a number is (<,>,=,!=) result
             if(validate.test.action == 'operator'){
                 if(!validate.test.operator){
                     console.error('With using operator Action you need to set validate.test.operator (<,>,=,!=)');
@@ -112,7 +132,8 @@ let runTestFramework = function(microservice){
                     callback();
                 }
             }else{
-                console.error('The Validation defined in JSON is invalid referrer to "usage/validations" File')
+                //Validation to check is not in the validation mapping file
+                console.error('The Validation defined in JSON is invalid referrer to "framework/validation.json" File')
                 assert.equal(true, false, 'No Validation Action Defined ('+validate.test.action+')');
                 callback(true);
             }
@@ -126,6 +147,7 @@ let runTestFramework = function(microservice){
 
         var sharedData = {};
         for (var testCaseLoop of test.testCases) {
+            //Console out testCase Information
             it('(EIH-'+test.id+') - Test Case: ' + testCaseLoop.name, function(done) {
                 this.timeout(80000000);
                 let runTest = test.testCases[i]; //Will need to loop through
@@ -139,17 +161,24 @@ let runTestFramework = function(microservice){
                              return 1;
                           return a.id === b.id ? 0 : -1;
                         });
+
+                        //Load Step Data
                         let step = runTest.steps[stepindex];
+
+                        //Load Controller of step type
                         let path = './test/' + step.type + '/controller.js';
                         let stepController = require(path);
+
+                        //Append All previous step results for shared data
                         step.params.shared = sharedData;
+
+                        //Append preSetup data to be used with shared data flag as Step: preSetup
                         step.params.shared.preSetup = preSetup;
                         adminFunctions.sharedDataCheck(step.params, function(options){
-//                            if(runExtraLogs){
-//                                console.log(formatJson.diffy(options));
-//                                console.log('Steps to Preform:');
-//                                console.log(formatJson.diffy(options.params));
-//                            }
+                            if(runExtraLogs){
+                                console.log('Steps to Preform:');
+                                console.log(formatJson.diffy(options));
+                            }
                             stepController.controller(driver, options, function(driver, sharedResult, error) {
                                 function writeScreenshot(data, name) {
                                   name = name || 'default.png';
@@ -160,6 +189,7 @@ let runTestFramework = function(microservice){
                                   writeScreenshot(data, 'testCaseID_'+test.id+'-Step_'+stepindex+'.png');
                                 });
 
+                                //Stop Step and display error that came back from controller
                                 if(error){
                                     console.error("Step: "+stepindex);
                                     console.error("Options: "+ JSON.stringify(step));
@@ -168,33 +198,49 @@ let runTestFramework = function(microservice){
                                     assert.equal(true, false, 'Error on Step: '+ stepindex);
                                     done();
                                 }
+
+                                //Added result from controller to global Shared Data for Next loop append
                                 if(sharedResult)
                                     sharedData['step'+stepindex] = sharedResult;
 
                                 //Check if Test is a validation also
                                 if((step.tests) && (step.tests[0])){
+
+                                    //StepTests - driver, web driver - index test location in array
                                     let runStepTest = function(driver, index){
+
+                                        //Pass Step information to Step Index
                                         runAssertions(driver, { 'test': step.tests[index] }, sharedResult, function(end){
-                                        //Run all validations in a loop
+
+                                           //Run all validations in a loop
                                            if(end){
+                                              //Assertion check comeback with end all tests and step
                                               done();
                                             }else if(step.tests[(index+1)]){
+                                                //Run Next Test incrementing index
                                                 runStepTest(driver, (index+1));
                                             }else{
+                                                //Check for Next step and if so Run next step in json
                                                 if (runTest.steps[stepindex + 1])
                                                     runSteps((stepindex + 1));
-                                                else{ 
+                                                else{
+                                                    //No More Steps in JSON file so increment testcase counter
+                                                    //Call done for mocha to run next testcase
                                                     i = i + 1;
                                                     done();
 												}
                                             }
                                         });
                                     }
+
+                                    //Run step Tests
                                     runStepTest(driver, 0);
                                 }else{
+                                    //Step has no Test so Go to next step in list
                                     if (runTest.steps[stepindex + 1]) 
                                         runSteps((stepindex + 1));
                                     else {
+                                        // End of steps run next testcase
                                         i = i + 1;
                                         done();
 									}
@@ -203,6 +249,7 @@ let runTestFramework = function(microservice){
                         });
                     }
                 }
+                //Start Running steps
                 runSteps(0);
             });
         }
