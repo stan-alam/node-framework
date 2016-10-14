9e3027440cb2b00834d1703de31bb1958f7bca15
Stanislaw Alam <stan.alam@gmail.com>
2016-10-14 11:21:12 -0400

    Clean up some logs

--- test/hub-admin-api/controller.js
+++ test/hub-admin-api/controller.js
@@ -9,14 +9,11 @@ const _ = require('lodash'),
     adminFunctions = require('../../lib/adminFunctions');
 
 let controller = function(driver, options, callback) {
-    console.log("These are the options for line 10" + JSON.stringify(options));
    authFunctions.getUiToken(driver, function(Token) {
-
         if (options.action == 'createApplications') {
             configFunctions.createApplications(options.applications.length, Token, options.applications, [], function(error, applications) {
                 callback(driver, applications);
             });
-
         } else if (options.action == 'setPermissionsStatusCode'){
             authFunctions.getEPAtoken(Token, function(error, jwt){
                 adminFunctions.allTenantsFlag(jwt, options.setPermissions, function(error, result) {
@@ -30,13 +27,11 @@ let controller = function(driver, options, callback) {
         } else if (options.action == 'setPermissionsStatusText'){
             authFunctions.getEPAtoken(Token, function(error, jwt){
                 adminFunctions.allTenantsFlag(jwt, options.setPermissions, function(error, result) {
-                     console.log("This is the options " + JSON.stringify(options));
                      callback(driver, {'text': result.body.message }, error);
                 });
             });
         } else if (options.action == 'setInvalidEndPoint'){
             authFunctions.openJwt(Token, function(error, openToken){
-                console.error('openToken: '+ JSON.stringify(openToken))
                 errorFunctions.sendMessageBadEndpoint(Token, (options.endPoint + openToken.tenant.id), function(error, result) {
                      if(error){
                         callback(driver, {'text': error.statusCode });
@@ -45,13 +40,8 @@ let controller = function(driver, options, callback) {
                      }
                 });
             });
-
-
-
-
         } else if (options.action == 'noTenantIDEndPoint') {
             authFunctions.openJwt(Token, function(error, openToken){
-                console.error('openToken: '+ JSON.stringify(openToken))
                 errorFunctions.sendMessageBadEndpointPost(Token, options.endPoint, function(error) {
                      if(error){
                         callback(driver, {'text': error.statusCode });
@@ -59,14 +49,8 @@ let controller = function(driver, options, callback) {
                 });
             });
         } else if (options.action == 'sendPermissionsInvalidPayload'){
-
-            console.log("This is the token" + Token);
-
             authFunctions.getEPAtoken(Token, function(error, jwt) {
                 authFunctions.openJwt(Token, function(error, openToken) {
-
-                console.log("This is the token from thisToken " + Token);
-
                 errorFunctions.sendPermissionsInvalidPayload(jwt, (options.endPoint + openToken.tenant.id), function(error, result) {
                      if(error){
                         callback(driver, {'text': error.statusCode });
@@ -77,14 +61,8 @@ let controller = function(driver, options, callback) {
             });
           });
         } else if (options.action == 'sendPermissionsNoPayload'){
-
-            console.log("This is the token" + Token);
-
             authFunctions.getEPAtoken(Token, function(error, jwt) {
                 authFunctions.openJwt(Token, function(error, openToken) {
-
-                console.log("This is the token from thisToken " + Token);
-
                 errorFunctions.sendPermissionsInvalidPayload(jwt, (options.endPoint + openToken.tenant.id), function(error, result) {
                      if(error){
                         callback(driver, {'text': error.statusCode });
